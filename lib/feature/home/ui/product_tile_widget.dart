7c050654ca73b5b19d871352cbda145a40e12456
Saugat-Shrestha <Shresthaasaugat31@gmail.com>
2024-03-07 21:50:37 +0545

    waiting for boards to finish

--- lib/feature/home/ui/product_tile_widget.dart
+++ lib/feature/home/ui/product_tile_widget.dart
@@ -8,15 +8,26 @@ class ProductTileWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.black),
+        image: DecorationImage(
+          image: NetworkImage(productDataModel.imageURL),
+        ),
+      ),
       margin: EdgeInsets.all(8),
       padding: EdgeInsets.all(8),
       child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             height: 200,
             width: double.infinity,
           ),
-          Text(productDataModel.name),
+          Text(
+            productDataModel.name,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
           Text(productDataModel.description),
         ],
       ),
